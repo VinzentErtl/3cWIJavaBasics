12e66789949bf1e777b5ced59d1b978ee542e3e3
vinzent <vinzent.ertl@htldornbirn.at>
2022-09-28 11:12:58 +0200

    init

---
+++ src/ifclauses/ifclauses.java
@@ -0,0 +1,21 @@
+package ifclauses;
+
+import java.util.Random;
+
+public class ifclauses {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        } else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medium");
+
+        } else {
+            System.out.println("Large");
+        }
+
+    }
+}
