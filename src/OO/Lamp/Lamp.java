6855ba5966c2266cc820b77650106af2cd15d2a4
vinzent <vinzent.ertl@htldornbirn.at>
2023-03-16 09:30:59 +0100

    11 Lamp

--- src/OO/Lamp/Lamp.java
+++ src/OO/Lamp/Lamp.java
@@ -4,8 +4,18 @@ import java.util.List;
 
 public class Lamp {
 
-    private List<Annealing_element> annealing_elements;
-    Lamp(int annealing_elements) {
-        this.annealing_elements = annealing_elements;
+    private String Annealing_element1;
+    private String Annealing_element2;
+    private String Annealing_element3;
+
+
+    public Lamp(String annealing_element1, String annealing_element2, String annealing_element3) {
+        Annealing_element1 = annealing_element1;
+        Annealing_element2 = annealing_element2;
+        Annealing_element3 = annealing_element3;
+    }
+
+    public void addLightElement(Annealing_element annealing_element){
+        this.
     }
 }
--- src/OO/Lamp/Main.java
+++ src/OO/Lamp/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Lamp l1 = new Lamp(7);
+
 
         Annealing_element a1 = new Annealing_element(20,"Nr1","white","ein");
         Annealing_element a2 = new Annealing_element(20,"Nr2","white","ein");
